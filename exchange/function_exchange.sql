15f66537deebb92260ea31406c18a738a0541068
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-02-13 14:22:16 +0330

    rename schema name

--- exchange/function_exchange.sql
+++ exchange/function_exchange.sql
@@ -11,8 +11,8 @@ AS $function$
  	 if CAST(check_quantity_for_deal.order_sell->>'quantity' AS INTEGER) = CAST(check_quantity_for_deal.order_buy->>'quantity' AS INTEGER) then
  	 	
  	 
- 	 	update mydata.orders set status = 2 where id=CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER); -- 2:SELL_FINISH 
- 	 	update mydata.orders set status = 1 where id=CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER); -- 1:BUY_FINISH 
+ 	 	update exchange.orders set status = 2 where id=CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER); -- 2:SELL_FINISH 
+ 	 	update exchange.orders set status = 1 where id=CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER); -- 1:BUY_FINISH 
  	 	
  	 	amount = CAST(check_quantity_for_deal.order_sell->>'quantity' AS INTEGER) * CAST(check_quantity_for_deal.order_sell->>'price' AS INTEGER);
  	 
@@ -26,15 +26,15 @@ AS $function$
 		difference_quantity = CAST(check_quantity_for_deal.order_sell->>'quantity' AS INTEGER) - CAST(check_quantity_for_deal.order_buy->>'quantity' AS INTEGER);
         amount = difference_quantity * CAST(check_quantity_for_deal.order_sell->>'price' AS INTEGER);
 
-        update mydata.orders set status = 5,user_id_to = CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER); -- 5:SELL_FIN_SUB
-        update mydata.orders set status = 1,user_id_to = CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER); -- 1:BUY_FINISH
+        update exchange.orders set status = 5,user_id_to = CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER); -- 5:SELL_FIN_SUB
+        update exchange.orders set status = 1,user_id_to = CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER); -- 1:BUY_FINISH
     
         update basic_auth.users set "accountBalance"="accountBalance" + amount where id=CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER);
  	 	update basic_auth.users set "accountBalance"="accountBalance" - amount where id=CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER);
 		
         --4:SELL_PENDING
  	 	--create sub order
-        insert into mydata.orders("price","quantity","created_at","action","status","sub_order_id","symbol_id","user_id_from")
+        insert into exchange.orders("price","quantity","created_at","action","status","sub_order_id","symbol_id","user_id_from")
    			values (CAST(check_quantity_for_deal.order_sell->>'price' AS INTEGER),difference_quantity,now(),
    					CAST(check_quantity_for_deal.order_sell->>'action' AS INTEGER),4,CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER),
    					CAST(check_quantity_for_deal.order_sell->>'symbol_id' AS INTEGER),CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER));
@@ -45,15 +45,15 @@ AS $function$
 		difference_quantity = CAST(check_quantity_for_deal.order_buy->>'quantity' AS INTEGER) - CAST(check_quantity_for_deal.order_sell->>'quantity' AS INTEGER);
         amount = difference_quantity * CAST(check_quantity_for_deal.order_sell->>'price' AS INTEGER);
 
-        update mydata.orders set status = 2,user_id_to = CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER); -- 2:SELL_FINISH
-        update mydata.orders set status = 6,user_id_to = CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER); -- 6:BUY_FIN_SUB
+        update exchange.orders set status = 2,user_id_to = CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_sell->>'id' AS INTEGER); -- 2:SELL_FINISH
+        update exchange.orders set status = 6,user_id_to = CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER) where id=CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER); -- 6:BUY_FIN_SUB
     
         update basic_auth.users set "accountBalance"="accountBalance" + amount where id=CAST(check_quantity_for_deal.order_sell->>'user_id_from' AS INTEGER);
  	 	update basic_auth.users set "accountBalance"="accountBalance" - amount where id=CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER);
 		
         --3:BUY_PENDING
  	 	--create sub order
-        insert into mydata.orders("price","quantity","created_at","action","status","sub_order_id","symbol_id","user_id_from")
+        insert into exchange.orders("price","quantity","created_at","action","status","sub_order_id","symbol_id","user_id_from")
    			values (CAST(check_quantity_for_deal.order_buy->>'price' AS INTEGER),difference_quantity,now(),
    					CAST(check_quantity_for_deal.order_buy->>'action' AS INTEGER),3,CAST(check_quantity_for_deal.order_buy->>'id' AS INTEGER),
    					CAST(check_quantity_for_deal.order_buy->>'symbol_id' AS INTEGER),CAST(check_quantity_for_deal.order_buy->>'user_id_from' AS INTEGER));
@@ -81,14 +81,14 @@ AS $function$
 	 if CAST(find_match_order.order_->>'action' AS INTEGER)=2 then -- 2:SELL     
 	    -- status=3:BUY_PENDING
 	 	cnt=0;
-	 	select count(*) into cnt from mydata.orders o
+	 	select count(*) into cnt from exchange.orders o
 			    where o.symbol_id=CAST(find_match_order.order_->>'symbol_id' AS INTEGER) and
 			   			o.status=3 and o.price=CAST(find_match_order.order_->>'price' AS INTEGER);
 		if cnt=0 then 
 			return json_build_object ('msg', 'buy pending list is empty');
 		end if;
 	
-	 	select (row_to_json(o.*)) into order_buy from mydata.orders o
+	 	select (row_to_json(o.*)) into order_buy from exchange.orders o
 	    where o.symbol_id=CAST(find_match_order.order_->>'symbol_id' AS INTEGER) and
 	   			o.status=3 and o.price=CAST(find_match_order.order_->>'price' AS INTEGER)
 	   	limit 1;
@@ -99,14 +99,14 @@ AS $function$
 	 elseif  CAST(find_match_order.order_->>'action' AS INTEGER)=1 then -- 1:BUY
 	 -- status=4:SELL_PENDING
 	 	cnt=0;
-	 	select count(*) into cnt from mydata.orders o
+	 	select count(*) into cnt from exchange.orders o
 			    where o.symbol_id=CAST(find_match_order.order_->>'symbol_id' AS INTEGER) and
 			   			o.status=4 and o.price=CAST(find_match_order.order_->>'price' AS INTEGER);
 		if cnt=0 then 
 			return json_build_object ('msg', 'sell pending list is empty');
 		end if;
 	
-	 	select (row_to_json(o.*)) into order_sell from mydata.orders o
+	 	select (row_to_json(o.*)) into order_sell from exchange.orders o
 	    where o.symbol_id=CAST(find_match_order.order_->>'symbol_id' AS INTEGER) and
 	   			o.status=4 and o.price=CAST(find_match_order.order_->>'price' AS INTEGER)
 	   	limit 1;
@@ -145,13 +145,13 @@ AS $function$
  
 	-- validation before sell
 	if validation_order.action_ = 2 then -- 2:SELL
-	     select coalesce(sum(o.quantity),0) into toatl_quantity_symbol_user from mydata.orders o
+	     select coalesce(sum(o.quantity),0) into toatl_quantity_symbol_user from exchange.orders o
 			where o.user_id_from = user_id and o.symbol_id = validation_order.symbol_id and o.status = 1; -- 1:BUY_FINISH
 	    if toatl_quantity_symbol_user < validation_order.quantity  then
 	        return json_build_object('msg','The number of symbol shares is not enough for sell. you have '||toatl_quantity_symbol_user::text||' shares');
 		end if;
 	
-		select coalesce(sum(o.quantity),0) into quantity_symbol_user_sell_pending from mydata.orders o
+		select coalesce(sum(o.quantity),0) into quantity_symbol_user_sell_pending from exchange.orders o
 			where o.user_id_from = user_id and o.symbol_id = validation_order.symbol_id and o.status = 4; -- 4:SELL_PENDING
 		if toatl_quantity_symbol_user < quantity_symbol_user_sell_pending + validation_order.quantity then
 		    return json_build_object('msg','some of order in sell queue & enter quantity should be less than '||toatl_quantity_symbol_user::text);
@@ -193,7 +193,7 @@ AS $function$
         raise exception 'numberOfUser must be smaller than %', count_of_user + 1;
     end if;
 	select s."numberOfAllAvailableSymbol",s.price,id into numberOfAllAvailableSymbol,price,symbol_id 
-		from mydata.symbol s where s."symbolName" = ipo.symbol_name;
+		from exchange.symbol s where s."symbolName" = ipo.symbol_name;
 	
     if  numberOfAllAvailableSymbol = 0 then
         raise exception 'IPO is closed';
@@ -222,14 +222,14 @@ AS $function$
             number_of_shares = number_of_shares + extra_shares;
         end if;
 		
-		insert into mydata.payment("amount", "created_at", "user_id", "action") values (money_need,NOW(),user_id,1); -- 1 refer to DEPOSIT in mydata.payment_action
+		insert into exchange.payment("amount", "created_at", "user_id", "action") values (money_need,NOW(),user_id,1); -- 1 refer to DEPOSIT in exchange.payment_action
 
-		insert into mydata.orders("price","quantity","created_at","action","status","symbol_id","user_id_from","user_id_to")
+		insert into exchange.orders("price","quantity","created_at","action","status","symbol_id","user_id_from","user_id_to")
 			values (price,number_of_shares,NOW(),1,1,symbol_id,user_id,23);
         
-        insert into mydata.payment("amount", "created_at", "user_id", "action") values (money_need,NOW(),user_id,2); -- 1 refer to WITHDRAWAL in mydata.payment_action
+        insert into exchange.payment("amount", "created_at", "user_id", "action") values (money_need,NOW(),user_id,2); -- 1 refer to WITHDRAWAL in exchange.payment_action
         numberOfAllAvailableSymbol = numberOfAllAvailableSymbol - number_of_shares;	
-        update  mydata.symbol set "numberOfAllAvailableSymbol" = numberOfAllAvailableSymbol where id = symbol_id ;
+        update  exchange.symbol set "numberOfAllAvailableSymbol" = numberOfAllAvailableSymbol where id = symbol_id ;
 		i = i+1;
 	 end loop;
 	 return i;
