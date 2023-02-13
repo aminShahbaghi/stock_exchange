a7630d9c18b5d0677f2e75dc1b2261dc510613fa
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-02-13 12:58:49 +0330

    create table symbol, order_action, order_status, orders in  schema exchange

---
+++ exchange/table_exchange.sql
@@ -0,0 +1,41 @@
+CREATE TABLE exchange."symbol" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "full_name" varchar(100) NOT NULL,
+    "price" integer NOT NULL CHECK ("price" >= 0),
+    "volume" integer NOT NULL CHECK ("volume" >= 0),
+    "numberOfAllAvailableSymbol" integer NOT NULL CHECK ("numberOfAllAvailableSymbol" >= 0),
+    "symbolName" varchar(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE exchange."order_action" (
+  "id" serial NOT NULL PRIMARY KEY,
+  "title" varchar(10) NOT NULL
+    
+);
+
+CREATE TABLE exchange."order_status" (
+  "id" serial NOT NULL PRIMARY KEY,
+  "title" varchar(15) NOT NULL
+    
+);
+
+CREATE TABLE exchange."orders" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "price" integer NOT NULL CHECK ("price" >= 0),
+    "quantity" smallint NOT NULL CHECK ("quantity" >= 0),
+    "created_at" timestamp  NOT NULL,
+    "action" int2 NOT NULL,
+    "status" int2 NOT NULL,
+    "sub_order_id" bigint NULL,
+    "symbol_id" bigint NOT NULL,
+    "user_id_from" bigint NOT NULL,
+    "user_id_to" bigint NULL,
+
+    FOREIGN KEY (symbol_id) REFERENCES mydata.symbol (id),
+    FOREIGN KEY (sub_order_id) REFERENCES mydata.orders (id),
+    FOREIGN KEY (user_id_from) REFERENCES basic_auth.users (id),
+    FOREIGN KEY (user_id_to) REFERENCES basic_auth.users (id),
+    FOREIGN KEY ("action") REFERENCES mydata.order_action(id),
+    FOREIGN KEY (status) REFERENCES mydata.order_status(id)
+    
+);
