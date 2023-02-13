14845570b9f2260fde51b92892a79d141a5c755a
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-02-13 12:57:01 +0330

    set id using pg_get_serial_sequence

--- payment/insert_data_payment.sql
+++ payment/insert_data_payment.sql
@@ -3,3 +3,13 @@ INSERT INTO payment.payment (amount,created_at,user_id,"action")
 
 insert into payment.payment_action
 	select * from mydata.payment_action ;
+	
+SELECT setval(pg_get_serial_sequence('payment.payment', 'id')
+            , COALESCE(max(id) + 1, 1)
+            , false)
+FROM   payment.payment;
+
+SELECT setval(pg_get_serial_sequence('payment.payment_action', 'id')
+            , COALESCE(max(id) + 1, 1)
+            , false)
+FROM   payment.payment_action;
