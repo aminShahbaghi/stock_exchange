cc465dc8d8d311249eb3f852dc38e29d11dd7e46
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-02-21 15:55:39 +0330

    create function edit order
    user can edit price or quantity or both of them

--- api/function_api.sql
+++ api/function_api.sql
@@ -214,3 +214,99 @@ $function$
 ;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION api.edit_order(order_id integer, price integer=-1, quantity integer=-1)
+ RETURNS json
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+ declare
+ 	user_id int;
+ 	uname text;
+	order_old record;
+	acount_balance int8;
+ begin
+	 
+ 	if current_setting('request.jwt.claims', true)::json->>'role' = 'web_anon' then 
+		return json_build_object('msg','token required!');
+	end if;
+
+	uname := current_setting('request.jwt.claims', true)::json->>'role';
+
+
+	select id into user_id from basic_auth.users u where u.username = uname;
+	if user_id is null then
+		return json_build_object ('msg', 'fail!');
+	end if;
+	
+	select o.* into order_old from exchange.orders o 
+		where o.id = edit_order.order_id and (o.status = 'BUY_PENDING' or o.status = 'SELL_PENDING');
+	
+	if order_old is null then
+		return json_build_object ('msg', 'there is no order!');
+	end if;
+	
+	if edit_order.price = -1 and edit_order.quantity = -1 then
+		raise exception 'invalid input'
+				using hint = 'price or quantity required!';
+	end if;
+	return json_build_object ('msg', order_old);
+	-- update price and quantity
+	if edit_order.price <> -1 and edit_order.quantity <> -1 and edit_order.price != order_old.price then
+		if order_old.action = 'BUY' then
+			select "accountBalance" into acount_balance from basic_auth.users where id = user_id;
+			if acount_balance < edit_order.price * edit_order.quantity then
+				raise exception 'acountbalance is not enough'
+				using hint = 'multiply price and quantity should be greater than or equall to acountbalance';
+			end if;
+		end if;
+	
+		update exchange.orders set price = edit_order.price, quantity = edit_order.quantity , created_at = now()
+			where id = edit_order.order_id;
+		
+		insert into exchange.orders_history ("order_id","order_price","order_quantity","order_created_at") 
+			values (order_old.id, order_old.price, order_old.quantity, order_old.created_at);
+		
+		return json_build_object ('msg', 'update quantity and price');
+	end if;
+	
+	-- update price	
+	if edit_order.price <> -1 and edit_order.price != order_old.price then
+		if order_old.action = 'BUY' then
+			select "accountBalance" into acount_balance from basic_auth.users where id = user_id;
+			if acount_balance < edit_order.price * order_old.quantity then
+				raise exception 'acountbalance is not enough'
+				using hint = 'multiply price and quantity should be greater than or equall to acountbalance';
+			end if;
+		end if;
+	
+		update exchange.orders set price = edit_order.price , created_at = now()
+			where id = edit_order.order_id;
+		
+		insert into exchange.orders_history ("order_id","order_price","order_quantity","order_created_at") 
+			values (order_old.id, order_old.price,null, order_old.created_at);
+	return json_build_object ('msg', 'update price');
+	end if;
+
+
+	-- update quantity	
+	if edit_order.quantity <> -1 and edit_order.quantity != order_old.quantity then
+		if order_old.action = 'BUY' then
+			select "accountBalance" into acount_balance from basic_auth.users where id = user_id;
+			if acount_balance < edit_order.quantity * order_old.price then
+				raise exception 'acountbalance is not enough'
+				using hint = 'multiply price and quantity should be greater than or equall to acountbalance';
+			end if;
+		end if;
+	
+		update exchange.orders set quantity = edit_order.quantity , created_at = now()
+			where id = edit_order.order_id;
+		insert into exchange.orders_history ("order_id","order_price","order_quantity","order_created_at") 
+			values (order_old.id, null, order_old.quantity, order_old.created_at);
+	return json_build_object ('msg', 'update quantity');
+	end if;
+
+	return json_build_object ('msg', 'quantity or price not change ');
+    
+ end;
+$function$
+;
