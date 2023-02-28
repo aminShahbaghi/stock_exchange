8ba55421e04639fd749d0141b97aa6d3a4f7f7da
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-02-28 04:11:23 +0330

    create view trade

---
+++ api/view_api.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW api.trade
+WITH(security_invoker=on)
+AS SELECT o.id,
+    o.price,
+    o.quantity,
+    o.created_at,
+    o.action,
+    o.status,
+    s."symbolName"
+   FROM exchange.orders o
+     JOIN exchange.symbol s ON o.symbol_id = s.id;
