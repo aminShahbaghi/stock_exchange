9b90d2106aa0fd94ef66479b7f69e8c3ea2227cc
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-03-11 13:02:46 +0330

    add column status to payment table in schema payment

--- payment/type_payment.sql
+++ payment/type_payment.sql
@@ -1,1 +1,10 @@
 CREATE TYPE payment.payment_action_enum AS ENUM ('DEPOSIT', 'WITHDRAWAL');
+
+----------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE payment.payment ADD status payment."payment_status_enum" DEFAULT 'PENDING'::payment."payment_status_enum";
+
+CREATE TYPE payment."payment_status_enum" AS ENUM (
+	'ACCEPT',
+	'REJECT',
+	'PENDING');
