96ccb4b2607a7648522010ddf36b2969e9c3a9e7
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-03-11 13:14:18 +0330

    create view for show info of payment slip

--- api/view_api.sql
+++ api/view_api.sql
@@ -10,3 +10,21 @@ AS SELECT o.id,
    FROM exchange.orders o
      JOIN exchange.symbol s ON o.symbol_id = s.id
    WHERE o.status not in('BUY_PENDING','SELL_PENDING');
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW api.info_payment_slip
+WITH(security_invoker=on)
+AS SELECT p.id,
+    p.amount,
+    p.created_at,
+    p.user_id,
+    p.action,
+    p.status,
+    ps.pk_fk_payment_id,
+    ps.account_number,
+    ps.payment_code,
+    ps.shaba_number,
+    ps.image_id
+   FROM payment.payment p
+     JOIN payment.payment_slip ps ON p.id = ps.pk_fk_payment_id;
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
