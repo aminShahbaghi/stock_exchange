a99b1470b91d06cfee7cc4adb3e6764754215689
aminShahbaghi <a.shahbaghi@yahoo.com>
2023-03-11 12:03:28 +0330

    return trade that status is finish

--- api/view_api.sql
+++ api/view_api.sql
@@ -8,4 +8,5 @@ AS SELECT o.id,
     o.status,
     s."symbolName"
    FROM exchange.orders o
-     JOIN exchange.symbol s ON o.symbol_id = s.id;
+     JOIN exchange.symbol s ON o.symbol_id = s.id
+   WHERE o.status not in('BUY_PENDING','SELL_PENDING');
